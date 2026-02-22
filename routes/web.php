ca0c95a478b20971841b9e00026a0451b4d0dd9c
xChristian2 <aquino315532@gmail.com>
2026-02-22 16:28:38 +0800

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {return view('Home');});
+// Home
+Route::get('/', function () {
+    return view('Home');
+});
 
+// Authentication Routes
 Route::get('/Logreg', [AuthController::class, 'showLogReg'])->name('logreg');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/auth/google', [AuthController::class, 'redirectToGoogle'])->name('auth.google');
-Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
+Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback'])->name('auth.google.callback');
+
+// Profile Routes (Protected - requires login)
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('/profile/change-password', [ProfileController::class, 'changePassword'])->name('profile.password');
+    Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
+});
+
+// Gallery route
+Route::get('/gallery', function () {return view('Gallery');})->name('gallery');
